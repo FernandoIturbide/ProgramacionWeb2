95f9d24565157ffc8975801326ad16caeb241700
FernandoIturbide <fernandoitrubide320@gmail.com>
2025-02-17 09:31:03 -0600

    clase 17/02

---
+++ Introduccion/js/app4.js
@@ -0,0 +1,23 @@
+/* let nombre ="Fernando";
+nombre = "Aaron";
+var edad=21;
+edad=22;
+const puerto = 3308;
+puerto = 3309; */
+
+/* console.log("Mi nombre es ");
+    let nombre ="Fernando";
+console.log(nombre);
+
+console.log("Mi edad es ");
+var edad=21;
+console.log(edad);
+
+var fer = "ITUA131DASWWE";
+ */
+
+/* let uno = "Hola";
+let dos = "Mundo";
+let unir = uno + " " + dos;
+console.log(unir); */
+

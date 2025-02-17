06eb4f0f3dcd0a6682e5662df7c49aa6797afcfe
FernandoIturbide <fernandoitrubide320@gmail.com>
2025-02-17 09:48:16 -0600

    17/02

---
+++ Introduccion/js/app5.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Ingrese su nombre");
+let apellido = prompt("Ingrese su apellido");
+let edad = prompt("Ingrese su edad");
+let localidad = prompt("Ingrese su localidad");
+let altura = prompt("Ingrese su altura");
+let peso = prompt("Ingrese su peso");
+console.log("1. Mi nombre es " + nombre + " " + apellido + "\n2. Tengo edad " + edad + " años\n3. Vivo en " + localidad + "\n4. Mido " + altura + "\n5. Peso " + peso + " kilogramos");

d0ed0b5a9a5ed1d795ad2e8dd59bac17cdf57b91
FernandoIturbide <fernandoitrubide320@gmail.com>
2025-02-17 10:22:10 -0600

    17/02

---
+++ Introduccion/js/app6.js
@@ -0,0 +1,16 @@
+let numero = prompt("Ingrese un numero");
+let numero2 = prompt("Ingrese otro numero");
+console.log(typeof numero);
+ let resultado = numero + numero2;
+resultado = parseInt(numero) + parseInt(numero2);
+console.log("El resultado de la suma es " + resultado);
+resultado =parseInt(numero) * parseInt(numero2);
+console.log("El resultado de la multiplicacion es " + resultado);
+resultado =parseInt(numero) - parseInt(numero2);
+console.log("El resultado de la resta es " + resultado);
+resultado =parseInt(numero) / parseInt(numero2);
+console.log("El resultado de la division es " + resultado);
+resultado =parseInt(numero) % parseInt(numero2);
+console.log("El resultado del modulo es " + resultado);
+console.log(typeof numero);
+// Output:
---
+++ Introduccion/js/app7.js
@@ -0,0 +1,18 @@
+//Relacionales
+let dato = 20 > 30;
+console.log(dato);
+dato = 20 < 30;
+console.log(dato);
+dato = 20 == 20;
+console.log(dato);
+// Output:
+
+//Logicos
+let dato2 = true && true;
+console.log(dato2);
+dato2 = true && false;
+console.log(dato2);
+dato2 = true || false;
+console.log(dato2);
+// Output:
+

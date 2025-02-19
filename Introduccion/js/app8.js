d55cdc6ed56ab6d27e4f96efa37ca15f3766d68f
FernandoIturbide <fernandoitrubide320@gmail.com>
2025-02-18 18:27:48 -0600

    Tarea

---
+++ Introduccion/js/app8.js
@@ -0,0 +1,28 @@
+console.log("Operadores Lógicos: ");
+//Operador AND
+console.log("   1. Operador AND");
+let And1 = false && false;
+let And2 = false && true;
+let And3 = true && false;
+let And4 = true && true;
+console.log("       False && False = "+ And1);
+console.log("       False && True = "+ And2);
+console.log("       True && False = "+ And3);
+console.log("       True && True = "+ And4);
+//Operador OR
+console.log("   2. Operador OR");
+let Or1 = false || false;
+let Or2 = false || true;
+let Or3 = true || false;
+let Or4 = true || true;
+console.log("       False || False = "+ Or1);
+console.log("       False || True = "+ Or2);
+console.log("       True || False = "+ Or3);
+console.log("       True || True = "+ Or4);
+//Operador NOT
+console.log("   3. Operador NOT");
+let Not1 = !true;
+let Not2 = !false;
+console.log("       !True = "+ Not1);
+console.log("       !False = "+ Not2);
+// Output:

5d0486b87f8e399f960efe251fe37a0d255235ab
FernandoIturbide <fernandoitrubide320@gmail.com>
2025-04-28 15:53:03 -0600

    Tarea API

--- Tarea_28_04_2025/app.js
+++ Tarea_28_04_2025/app.js
@@ -1,27 +1,14 @@
-function generarQR(){
-    let var1 = document.getElementById('input1').value.trim();
-    let var2 = document.getElementById('input2').value.trim();
-    let var3 = document.getElementById('input3').value.trim();
-    let var4 = document.getElementById('input4').value.trim();
-    var textoConcatenado = var1+","+var2+","+var3+","+var4;
-
-    if ( textoConcatenado === ',,' ){
-        alert("Favor de ingresar datos");
-    }else{
-        
-
-    let qrApi= 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data='+encodeURIComponent(textoConcatenado);
-    
-    let qrImg = document.createElement('img');
-    qrImg.src=qrApi;
-    qrImg.alt='Codigo de QR';
-    console.log(qrImg.src);
-    
-    qrImg.classList.add('qr-code');
-    
-    document.getElementById('qr-code').appendChild(qrImg);
-    
-
-
+function Enviar() {
+    var telefono = document.getElementById('input1').value.trim();
+    var mensaje = document.getElementById('input2').value.trim();
+    if (telefono === '' || mensaje === '') {
+      alert('Por favor completa ambos campos.');
+      return;
     }
-}
+    var codigoPais = "52"; 
+    var enlace = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
+  
+    // Abrir WhatsApp en una nueva pestaña
+    window.open(enlace, '_blank');
+  }
+   
